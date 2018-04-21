5090e44569e120507436f839a551b8865c7c9f6c
João Paulo <jpdias1997@hotmail.com>
2018-04-21 17:11:36 -0300

    Adição de Modelos do Restaurante

---
+++ src/java/Modelos/ItemPedido.java
@@ -0,0 +1,72 @@
+package Modelos;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+
+public class ItemPedido {
+
+	private Pedido pedido;
+
+	private Produto produto;
+
+	private int quantidade;
+
+
+    public double getValorTotal() {
+        DecimalFormat df = new DecimalFormat("#0.00");
+        df.setRoundingMode(RoundingMode.CEILING);
+        Number n = 0;
+        try{
+        n = df.parse(df.format(produto.getValor()*quantidade));
+        }catch (ParseException ex){
+            System.out.println("Erro na conversão");
+        }
+        return n.doubleValue();
+    }
+
+    public ItemPedido(Pedido pedido, Produto produto, int quantidade) {
+        this.pedido = pedido;
+        this.produto = produto;
+        setQuantidade(quantidade);
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        if(quantidade>0){
+        this.quantidade = quantidade;
+        }
+        else{
+            throw new NumberFormatException();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Item: "+getProduto().toString() + " x " + getQuantidade() + " = R$ " +getValorTotal();
+    }
+    
+    
+        
+        
+
+}
---
+++ src/java/Modelos/Produto.java
@@ -0,0 +1,100 @@
+package Modelos;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Produto {
+
+    private Integer id;
+
+    private String descricao;
+
+    private Double valor;
+
+    private static int gencodigo = 0;
+
+    public Produto() {
+    }
+    
+    
+
+    public Produto(String descricao, Double valor) {
+        this.id = gerarCodigo();
+        this.descricao = descricao;
+        setValor(valor);
+    }
+
+    public Produto(Integer id, String descricao, Double valor) {
+        this.id = id;
+        this.descricao = descricao;
+        setValor(valor);
+    }
+
+    public static int gerarCodigo() {
+        int cod = gencodigo;
+        gencodigo++;
+        return cod;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        if(valor>0){
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.CEILING);
+        Number n = 0;
+        try{
+        n = df.parse(df.format(valor));
+        }catch (ParseException ex){
+            System.out.println("Erro na conversão");
+        }
+        this.valor = n.doubleValue();
+    }
+        else{
+            throw new NumberFormatException();
+        }
+    }
+    @Override
+    public String toString() {
+        return "Descrição: " + descricao + "; Valor: R$ " + valor;
+    }
+    
+    public Produto clonar(){
+        Produto p = new Produto();
+        p.setId(this.id);
+        p.setDescricao(this.descricao);
+        p.setValor(this.valor);
+        return p;
+    }
+      public static List<Produto> getSampleData() {
+        List<Produto> ListProdutos = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Produto p = new Produto("Produto " + i, (45.8 * i+1) / 3);
+            ListProdutos.add(p);
+        }
+        return ListProdutos;
+
+    }
+}
